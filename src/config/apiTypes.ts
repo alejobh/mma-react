2d65e6f3e89b9fcee2c6a54d0324ac389128274c
MMarussi <jmmarussi32@gmail.com>
2021-05-04 12:10:00 -0300

    Moved api response type to apiTypes file

--- src/config/apiTypes.ts
+++ src/config/apiTypes.ts
@@ -1,3 +1,7 @@
 export type Error = {
   errors: Record<string, string[]> | string[];
 };
+
+export type Response = {
+  data: Record<string, string | number>;
+};
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,9 +1,5 @@
 import api from 'config/api';
-import { Error } from 'config/apiTypes';
+import { Error, Response } from 'config/apiTypes';
 import { SignUpValues } from 'utils/types';
 
-type SignUpResponse = {
-  data: Record<string, string | number>;
-};
-
-export const signUp = (data: SignUpValues) => api.post<SignUpResponse, Error>('/users', data);
+export const signUp = (data: SignUpValues) => api.post<Response, Error>('/users', data);
